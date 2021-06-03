91e57b781dc4a1566a4818a46eee6f119513a358
Giacomo <gc76@live.it>
2021-06-04 00:57:32 +0200

    GIACOMO - rimesso materiale

--- src/main/java/org/example/centrivaccinali/gui/ClientCV.java
+++ src/main/java/org/example/centrivaccinali/gui/ClientCV.java
@@ -30,43 +30,6 @@ public class ClientCV {
      */
     public static void main(String[] args) throws IOException {
 
-
-
-
-        final String path = "resources/org/example/centrivaccinali/gui/listacomuni.csv";
-        final File jarFile = new File(ClientCV.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-
-        if(jarFile.isFile()) {  // Run with JAR file
-            final JarFile jar = new JarFile(jarFile);
-            final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
-            while(entries.hasMoreElements()) {
-                final String name = entries.nextElement().getName();
-                if (name.startsWith(path + "/")) { //filter according to the path
-                    System.out.println(name);
-                }
-            }
-            jar.close();
-        } else { // Run with IDE
-            final URL url = ClientCV.class.getResource("/" + path);
-            if (url != null) {
-                try {
-                    final File apps = new File(url.toURI());
-                    for (File app : apps.listFiles()) {
-                        System.out.println(app);
-                    }
-                } catch (URISyntaxException ex) {
-                    // never happens
-                }
-            }
-        }
-
-
-
-
-
-
-
-
         ClientCVMain.main(args); // fa partire l'intrerfaccia grafica
 
         //legge il file policy nella cartella client con i permessi di chi puo usare l'app
