648db457e96d3e7bd85cb89e0c83ba5269a1226d
Astargisco <edubini@studenti.uninsubria.it>
2021-06-03 23:40:14 +0200

    EMANUELE & DAVIDE -lettura del file listacomuni.csv sia da jar che da ide

--- src/main/java/org/example/centrivaccinali/gui/ClientCV.java
+++ src/main/java/org/example/centrivaccinali/gui/ClientCV.java
@@ -10,14 +10,13 @@
 
 package org.example.centrivaccinali.gui;
 
-import org.example.common.ProgUtili;
-import org.example.common.exceptions.ServerNotFoundException;
-import org.example.serverCV.ServerCV;
-import org.example.serverCV.gui.ServerCVMain;
-
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Classe <code>ClientCV</code> che costituisce l'entry point dell'applicazione.
@@ -29,7 +28,44 @@ public class ClientCV {
     /**
      * Metodo <code>Main</code> dell'applicazione ClientCV
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+
+
+        final String path = "resources/org/example/centrivaccinali/gui/listacomuni.csv";
+        final File jarFile = new File(ClientCV.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+
+        if(jarFile.isFile()) {  // Run with JAR file
+            final JarFile jar = new JarFile(jarFile);
+            final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+            while(entries.hasMoreElements()) {
+                final String name = entries.nextElement().getName();
+                if (name.startsWith(path + "/")) { //filter according to the path
+                    System.out.println(name);
+                }
+            }
+            jar.close();
+        } else { // Run with IDE
+            final URL url = ClientCV.class.getResource("/" + path);
+            if (url != null) {
+                try {
+                    final File apps = new File(url.toURI());
+                    for (File app : apps.listFiles()) {
+                        System.out.println(app);
+                    }
+                } catch (URISyntaxException ex) {
+                    // never happens
+                }
+            }
+        }
+
+
+
+
+
+
+
 
         ClientCVMain.main(args); // fa partire l'intrerfaccia grafica
 
