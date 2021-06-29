ccbdd37ce87226e4d1ade808583a1a9ef71b2b0f
Davide <dbancora@studenti.uninsubria.it>
2021-06-29 23:00:23 +0200

    DAVIDE & EMANUELE - Risolto problema ricerca (ora pulisce le ricerche precedenti) in 03CT_CercaCV.fxml - Pulizia classe Client03CT_CercaCV_Controller.java

--- src/main/java/org/example/centrivaccinali/gui/Client03CT_CercaCV_Controller.java
+++ src/main/java/org/example/centrivaccinali/gui/Client03CT_CercaCV_Controller.java
@@ -1,5 +1,6 @@
 package org.example.centrivaccinali.gui;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +9,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -29,6 +31,8 @@ import java.util.ResourceBundle;
 //public class Client03CT_CercaCV_Controller implements Initializable {
 public class Client03CT_CercaCV_Controller {
 
+    @FXML
+    public ScrollPane scroll;
     @FXML
     public Button bottoneRicerca;
     @FXML
@@ -57,32 +61,7 @@ public class Client03CT_CercaCV_Controller {
 
     public List<CentroVaccinale> ricerca() throws SQLException, RemoteException {
         String nome = nomeDaRicercare.getText().strip();
-
         System.err.println(nome);
-
-        //System.err.println(prova);
-        //ArrayList<CentroVaccinale> ct = new ArrayList<>();
-        /*ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 1", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Ospedaliero));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 2", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));
-        ct.add(new CentroVaccinale("006066a7-8726-47bc-8764-7fc58858d633", "prova 3", new Indirizzo(Qualificatore.Corso, "Basadonne", "129A", "Assago", 22072, "MI"), TipologiaCV.Hub));*/
         return ClientCVController.stub.cercaCentroVaccinaleNome(nome);
     }
 
@@ -94,83 +73,16 @@ public class Client03CT_CercaCV_Controller {
         ViaLabel.setText(indirizzo.getQualificatore() + " " + indirizzo.getNome() + " " + indirizzo.getNumeroCivico());
         IndirizzoLabel.setText(indirizzo.getComune() + " " + indirizzo.getCap() + " " + indirizzo.getProv());
         idCentroVacinaleLabel.setText(centroVaccinale.getIdCentroVacciale());
-        //image = new Image("src/main/resources/org/example/images/primula.png");
-        //CVImg.setImage(image);
-
-        //chosenCV.setStyle("-fx-background-color: #" + "95897f" + ";\n" +  //setta il colore di sfondo del CV scelto
-        // "    -fx-background-radius: 30;");
         chosenCV.setStyle("-fx-background-radius: 30;");
     }
 
-    /*@Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            centriVaccinali.addAll(ricerca());
-        } catch (Exception e) {
-            System.err.println("ERRORE NELLA CLASSE: Client03CT_CercaCV_Controller");
-            e.printStackTrace();
-        }
-
-        if (centriVaccinali.size() > 0) {
-            impostaCVscelto(centriVaccinali.get(0));
-            cercaCVListener = new CercaCVListener() {
-                @Override
-                public void onClickListener(CentroVaccinale centroVaccinale) {
-                    impostaCVscelto(centroVaccinale);
-                }
-            };
-        }
-
-
-
-        int column = 0;
-        int row = 1;
-        try {
-            for (int i = 0; i < centriVaccinali.size(); i++) {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(Client03CT_CercaCV_Controller.class.getResource("item.fxml"));
-                AnchorPane anchorPane = fxmlLoader.load();
-
-                ItemController itemController = fxmlLoader.getController();
-                itemController.setData(centriVaccinali.get(i),cercaCVListener);
-
-                if (column == 3) {
-                    column = 0;
-                    row++;
-                }
-
-                grid.add(anchorPane, column++, row); //(child,column,row)
-                //set grid width
-                grid.setMinWidth(Region.USE_COMPUTED_SIZE);
-                grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
-                grid.setMaxWidth(Region.USE_PREF_SIZE);
-
-                //set grid height
-                grid.setMinHeight(Region.USE_COMPUTED_SIZE);
-                grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
-                grid.setMaxHeight(Region.USE_PREF_SIZE);
-
-                GridPane.setMargin(anchorPane, new Insets(10));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
-
-    private void resetItem() { //todo sistemare il metodo per cancellare ogni elemento prima di una ricerca
-                /*while(grid.getRowConstraints().size() > 0) {
-            grid.getRowConstraints().remove(0);
-        }
-
-        while(grid.getColumnConstraints().size() > 0) {
-            grid.getColumnConstraints().remove(0);
-        }
-        grid.getChildren().removeAll();
-        System.err.println("CANCELLAZIONE GRIGLIA!!!");*/
+    private void resetItem() {
+        centriVaccinali.clear();    //Cancella l' ArrayList che contiene tutti i risultati ricevuti dal server
+        System.err.println("CANCELLAZIONE GRIGLIA!!!");
     }
 
     public void initialize(ActionEvent actionEvent) {
-        resetItem();
+
         try {
             centriVaccinali.addAll(ricerca());
         } catch (Exception e) {
@@ -192,9 +104,8 @@ public class Client03CT_CercaCV_Controller {
             int column = 0;
             int row = 1;
             try {
+                grid.getChildren().clear();     //Puliamo la GridPane prima di stampare i nuovi risultati di ricerca
                 for (int i = 0; i < centriVaccinali.size(); i++) {
-                /*FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("src/main/resources/org/example/centrivaccinali/gui/item.fxml"));*/
 
                     FXMLLoader fxmlLoader = new FXMLLoader(ClientCVMain.class.getResource("item.fxml"));
                     AnchorPane anchorPane = fxmlLoader.load();
@@ -206,7 +117,6 @@ public class Client03CT_CercaCV_Controller {
                         column = 0;
                         row++;
                     }
-
                     grid.add(anchorPane, column++, row); //(child,column,row)
                     //set grid width
                     grid.setMinWidth(Region.USE_COMPUTED_SIZE);
@@ -219,13 +129,22 @@ public class Client03CT_CercaCV_Controller {
                     grid.setMaxHeight(Region.USE_PREF_SIZE);
 
                     GridPane.setMargin(anchorPane, new Insets(10));
+
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        resetItem();
+    }
 
+    public void onClickQuit(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 
+    public void to_02CT_MainWindow(ActionEvent actionEvent) throws IOException {
+        ClientCVMain.setRoot("02CT_MainWindow");
+    }
 }
 
 
